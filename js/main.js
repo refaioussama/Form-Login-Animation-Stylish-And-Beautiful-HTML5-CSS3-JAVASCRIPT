c5bfca6e04262d3aa59c44d0092a68af6e6f3fba
refaioussama <42978000+refaioussama@users.noreply.github.com>
2018-09-17 16:37:58 +0100

    Form Login Animation Stylish

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+    var box = $('.box')
+
+        box.animate({
+            left: '48%'
+        }, 500);
+
+        box.animate({
+            top: '48%'
+        }, 500);
+
+        box.animate({
+            left: '36%'
+        }, 500);
+
+        box.animate({
+            width: '300px'
+        }, 500);
+
+        box.animate({
+            height: '300px'
+        }, 500);
+});
